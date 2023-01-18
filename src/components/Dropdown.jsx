cc9bf0d1192f08ebab5085bdde6bedd3ab34f129
Gordana <gordannaa4@gmail.com>
2023-01-18 19:38:07 +0100

    chas6

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,15 @@
+export const Dropdown = ({label, options, onChange}) => {
+
+    return <div>
+        <label>{label}</label>
+        <select 
+        onChange={onChange}
+        >
+            <option></option>
+            {options.map(optionElement => (
+            <option key={optionElement.value}>
+                {optionElement.name}
+            </option>))}
+        </select>
+    </div>
+}
