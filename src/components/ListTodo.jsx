6d23bbdae1d4baca9069fb4e759ef2277bfd3bd9
Gordana <gordannaa4@gmail.com>
2023-01-23 21:31:32 +0100

    chas8

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -1,10 +1,24 @@
-const ListTodo = (props) => {
-  const { todos, deleteTodo } = props;
+import { useState } from "react";
 
+const ListTodo = (props) => {
+  const { todos, deleteTodo, markAsCompleted, updateTodoTitle } = props;
+
+  const [displayCompleted, setDisplayCompleted] = useState(true);
+  const [editId, setEditId] = useState("");
+  const [editTitle, setEditTitle] = useState("");
   const style = {
     border: "1px solid black",
     borderCollapse: "collapse",
     padding: "5px",
+  };
+  const save = () => {
+    updateTodoTitle(editId, editTitle);
+    setEditId("");
+  };
+
+  const cancel = () => {
+    setEditId("");
+    setEditTitle("");
   };
   return (
     <div>
@@ -16,29 +30,67 @@ const ListTodo = (props) => {
             <td>ID</td>
             <td>Title</td>
             <td>Completed</td>
-            <td>Delete</td>
             <td>Compltete</td>
+            <td>Edit</td>
+            <td>Delete</td>
           </tr>
         </thead>
         <tbody>
-          {todos.map((todo) => (
-            <tr>
-              <td>{todo.userId}</td>
-              <td>{todo.id}</td>
-              <td>{todo.title}</td>
-              <td>
-                <input type="checkbox" checked={todo.completed} />
-              </td>
-              <td>
-                <button onClick={() => deleteTodo(todo.id)}>X</button>
-              </td>
-              <td>
-                <button>✓</button>
-              </td>
-            </tr>
-          ))}
+          {todos.map(
+            (todo) =>
+              (!todo.completed || displayCompleted) && (
+                <tr>
+                  <td>{todo.userId}</td>
+                  <td>{todo.id}</td>
+                  {editId && editId === todo.id ? (
+                    <td>
+                      <input
+                        type="title"
+                        value={editTitle}
+                        onChange={(e) => setEditTitle(e.target.value)}
+                        size={todo.title.length}
+                      />
+                      <input type="button" onClick={save} value="Save" />
+                      <input type="button" onClick={cancel} value="Cancel" />
+                    </td>
+                  ) : (
+                    <td className={todo.completed ? "mark-as-done" : ""}>
+                      {todo.title}
+                    </td>
+                  )}
+                  <td>
+                    <input type="checkbox" checked={todo.completed} />
+                  </td>
+                  <td>
+                    <button onClick={() => markAsCompleted(todo.id)}>✓</button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        setEditId(todo.id);
+                        setEditTitle(todo.title);
+                      }}
+                    >
+                      ✎
+                    </button>
+                  </td>
+                  <td>
+                    <button onClick={() => deleteTodo(todo.id)}>X</button>
+                  </td>
+                </tr>
+              )
+          )}
         </tbody>
       </table>
+
+      <div>
+        <label>Display Completed?</label>
+        <input
+          type="checkbox"
+          checked={displayCompleted}
+          onChange={() => setDisplayCompleted(!displayCompleted)}
+        />
+      </div>
     </div>
   );
 };
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -18,10 +18,36 @@ const Todo = () => {
     setTodos([...todos.filter((todo) => todo.id !== id)]);
   };
 
+  const markAsCompleted = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  };
+  const updateTodoTitle = (id, newTitle) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = newTitle;
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
     <div>
       <CreateTodo setTodos={setTodos} />
-      <ListTodo todos={todos} deleteTodo={deleteTodo} />
+      <ListTodo
+        todos={todos}
+        deleteTodo={deleteTodo}
+        markAsCompleted={markAsCompleted}
+        updateTodoTitle={updateTodoTitle}
+      />
     </div>
   );
 };
